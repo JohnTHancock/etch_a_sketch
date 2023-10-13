ec03454e311a8765956cd45e50c63dfe64166d8b
JT <143206481+JohnTHancock@users.noreply.github.com>
2023-10-13 10:21:37 -0400

    Add mouseover event to change cell colors in the grid
    Added a mouseover event listener to the javascript.js file that
    changes the class of the target div to 'blackCell' which is the same
    as the default except the background color is black instead of dark
    gray.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let container = document.getElementById('container');
 let input = 16;
 let cellSize = (960/input)-2;
+container.classList.add('blackCell');
 totalCells = input**2;
 
 for (let i=0; i< totalCells; i++) {
@@ -8,8 +9,10 @@ let gridCell = document.createElement('div');
 gridCell.style.width = cellSize + 'px';
 gridCell.style.height = cellSize + 'px';
 container.appendChild(gridCell);
-gridCell.setAttribute('class', 'cell');
+gridCell.setAttribute('class', 'baseCell');
 }
 
-console.log(cellSize);
-console.log(totalCells);
+container.addEventListener('mouseover', (event) => {
+    let target = event.target;
+    target.setAttribute('class', 'blackCell');
+});
