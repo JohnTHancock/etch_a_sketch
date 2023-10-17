29c8c6336fa2a1f8565a1095dbe83890b35f998e
JT <143206481+JohnTHancock@users.noreply.github.com>
2023-10-17 11:27:27 -0400

    Finish reset button
    Fixed the issues with the reset button by using
    container.replaceChildren() instead of .remove. Full program now works
    as expected.

--- javascript.js
+++ javascript.js
@@ -4,24 +4,24 @@ let input;
 let cellSize;
 let totalCells;
 let gridCell;
-let cells;
 let redBox = document.getElementById('redBox');
 
 container.classList.add('baseCell', 'blackCell');
+
 
 function loadNewGrid(input) {
     if (input === undefined || input === null) {
         input = 16;
     }
-    cellSize = (960/input)-2;
-    totalCells = input**2;
+    cellSize = (960 / input) - 2;
+    totalCells = input ** 2;
 
-    for (let i=0; i< totalCells; i++) {
-    let gridCell = document.createElement('div');
-    gridCell.style.width = cellSize + 'px';
-    gridCell.style.height = cellSize + 'px';
-    container.appendChild(gridCell);
-    gridCell.setAttribute('class', 'baseCell');
+    for (let i = 0; i < totalCells; i++) {
+        let gridCell = document.createElement('div');
+        gridCell.style.width = cellSize + 'px';
+        gridCell.style.height = cellSize + 'px';
+        container.appendChild(gridCell);
+        gridCell.setAttribute('class', 'baseCell');
     }
 }
 
@@ -38,9 +38,7 @@ resetButton.addEventListener('click', (event) => {
     if (input > 100 || input < 1 || isNaN(input)) {
         input = prompt('Please enter a number between 1 and 100.');
     } else {
-        cells = document.querySelectorAll('.baseCell');
-        console.log(input);
-        cells.remove(); //this is the problem. container.remove deletes the container but cells.remove returns as not a function
+        container.replaceChildren();
         loadNewGrid(input);
     }
 })
